b9e9d4d8a1756bd7c17b6e1915fbeeef1d7fd987
mokassino <prokurush@gmail.com>
2023-02-09 16:36:00 +0100

    improved searchbar, added more api results

--- app/controller/pymongo_interface.py
+++ app/controller/pymongo_interface.py
@@ -6,30 +6,58 @@ from pymongo import (
 class PymongoInterface:
     def __init__(self, string):
         self.CONNECTION_STRING = string 
-        self.client = MongoClient(string)
+        self.client = MongoClient(self.CONNECTION_STRING)
     def get_db(self):
         return self.client.pcbuilder
-    def get_processors(self):
-        return self.client.pcbuilder.processor
     def close(self):
         self.client.close()
 
-class SearchBarInterface(PymongoInterface):
+class SearchBarInterface(PymongoInterface): #sub class of PymongoInterface
+    def searchbar_query(self, collection, q):
+        # We only need a few camps from the bson query result of 
+        query = {}
+        l = list()
+
+        if collection.name == 'gpu' or collection.name == 'processor':
+            _filter = {'_id' : 1, 'brand' : 1}
+            l = list(map(lambda x : x['brand']+' '+ x['_id'], collection.find(query, _filter))) 
+            # collection.find() make a query on the database with the _filter and returns stuff
+            # the lambda function merges two camps from the json output and pass them to the map function
+            # i.e:  {_id: "5-5600" , brand: "amd ryzen"} --becomes-->  "amd ryzen 5-5600" # then convert back to a list
+        elif collection.name == 'motherboard':
+            _filter = {'_id' : 1, 'name' : 1, 'brand' : 1, }
+            l = list(map(lambda x : x['brand']+ ' '+ x['name'] + ' ' + x['_id'], collection.find(query, _filter))) 
+
+        return self.searchbar_filter(l, q)
+    
+    def searchbar_filter(self, input, q):
+        if (q != None or q != '') and len(input) > 0:
+            ll = list(filter( lambda i : q in i  ,input))
+            # filter for elements that match the request arguments
+            #i.e if q='amd ryzen', shows only amd ryzen cpu 
+            return ll
+        else:
+            return input
+
+
     def get_processors(self, q=None): #q is an optional argument
-        # We don't need everything in the search bar so i'll made a filter
-        query = {} # find everything
-        _filter = {'_id' : 1, 'brand' : 1}
+        collection = self.get_db().processor
 
-        p = self.client.pcbuilder.processor
+        return self.searchbar_query(collection, q)
+    
+    def get_gpu(self, q=None):
+        collection = self.get_db().gpu 
+        return self.searchbar_query(collection,q)
+    
+    def get_motherboard(self, q=None):
+        collection = self.get_db().motherboard
+        return self.searchbar_query(collection, q)
+    
+    def get_everything(self, q=None):
+        gpu = self.get_gpu(q)
+        cpu = self.get_processors(q)
+        mb = self.get_motherboard(q)
 
-        l = list(p.find(query, _filter))
-        ll = list(map(lambda x : x['brand']+' '+ x['_id'], l)) # merge two camps from the json output into a list of strings
-        # i.e:  {_id: "5 5600" , brand: "amd ryzen"} --becomes-->  "amd ryzen 5 5600"
-        # then convert back to a list
-        if q != None or q != '':
-            ll = list(filter( lambda i : q in i  ,ll)) # filter for elements that match the request arguments
-        #i.e if q='amd ryzen', shows only amd ryzen elements
-
-
-        return ll #return a list of specific camps from a JSON
+        l = list(gpu + cpu + mb)
+        return l
 
